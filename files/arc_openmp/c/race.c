30249d5acac1b80b134fae5866218185915c995a
Thomas Green <tomgreen66@hotmail.com>
2020-11-26 23:02:38 +0000

    Add some fixes.

--- files/arc_openmp/c/race.c
+++ files/arc_openmp/c/race.c
@@ -6,7 +6,14 @@ int main()
     srand (0);
     int i;
     int N=20;
+    int my_rand[N];
     int best_cost=RAND_MAX;
+
+    for (i=0; i<N; i++)
+    {
+      my_rand[i] = rand();
+    }
+
     #pragma omp parallel shared(best_cost)
     {
         #pragma omp for nowait
@@ -15,7 +22,7 @@ int main()
             int tid = omp_get_thread_num();
             int my_cost;
             // Be careful using rand in OpenMP parallel regions.
-            my_cost = rand() % 100;
+            my_cost = my_rand[i] % 100;
             printf("tid %i -  %i\n",tid,my_cost);
             //#pragma omp critical
             if(best_cost > my_cost)
--- files/arc_openmp/fortran/race.f90
+++ files/arc_openmp/fortran/race.f90
@@ -7,25 +7,28 @@ program hello_world
 
     integer :: i, best_cost, tid, my_cost
     integer, parameter :: N = 20
+    real    :: my_rand(N)
 
     ! Seed random number
     call random_seed(put=(/0/))
     best_cost=huge(best_cost)
+    ! Note check thread-safety of random generator or avoid altogether
+    ! using inside threads.
+    call random_number(my_rand)
 
     !  Fork a team of threads, with private versions of the declared variables.
     !$OMP PARALLEL SHARED(best_cost) PRIVATE(tid, my_cost)
 
     !$OMP DO
     do i = 1, N
-      ! Note check thread-safety of random generator.
-      call random_number(my_cost)
-      my_cost = floor(my_cost*100)
+      my_cost = floor(my_rand(i)*100)
       print *, "tid ", tid, " - ", tid, my_cost
-      !$OMP OMP CRITICAL
+      !$OMP CRITICAL
       if (best_cost > my_cost) then
         print *, "tid ", tid, " says that ", my_cost, " is lower than ", best_cost
         best_cost = my_cost
       end if
+      !$OMP END CRITICAL
     end do
     !$OMP END DO NOWAIT
 
